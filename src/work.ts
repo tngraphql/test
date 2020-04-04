7025ce7098ac8ed0e36d8b01c2be95f00f714626
nguyenpl117 <nguyenpl117@gmail.com>
2020-04-04 21:44:26 +0700

    asf

---
+++ src/work.ts
@@ -0,0 +1,12 @@
+/**
+ * (c) Phan Trung Nguyên <nguyenpl117@gmail.com>
+ * User: nguyenpl117
+ * Date: 4/4/2020
+ * Time: 9:42 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_PASSWORD);
